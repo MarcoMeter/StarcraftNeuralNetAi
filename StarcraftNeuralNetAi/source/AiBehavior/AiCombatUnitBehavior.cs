2ef57e564c3bcc29486232c907b99b61e6b45c11
Marco Pleines <m.pleines@devbeyond.de>
2016-04-17 13:13:01 +0200

    Started work on the ai behavior design. For some reasons units will always move to (0,0) after calling Attack, Rightclick or Move.

---
+++ StarcraftNeuralNetAi/source/AiBehavior/AiCombatUnitBehavior.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BroodWar;
+using BroodWar.Api;
+using BroodWar.Api.Enum;
+
+namespace NetworkTraining
+{
+    public class AiCombatUnitBehavior
+    {
+        #region Member
+        private Unit unit;
+        private SquadSupervisor supervisor;
+        private CombatUnitState currentState = CombatUnitState.Idle;
+        #endregion
+
+        #region Constructor
+        public AiCombatUnitBehavior(Unit unit, SquadSupervisor supervisor)
+        {
+            this.unit = unit;
+            this.supervisor = supervisor;
+        }
+        #endregion
+
+        #region public functions
+        // Getter
+        public Unit GetUnit()
+        {
+            return this.unit;
+        }
+
+        public SquadSupervisor GetSupervisor()
+        {
+            return this.supervisor;
+        }
+
+        public CombatUnitState GetCurrentState()
+        {
+            return this.currentState;
+        }
+
+        // Setter
+        public void ForceState(CombatUnitState state)
+        {
+            this.currentState = state;
+        }
+        #endregion
+
+        #region Behavior Logic
+        /// <summary>
+        /// The neural net will be executed each frame. Based on the decisions made by net net, the concerning state of the finite state machine will be executed.
+        /// </summary>
+        public void OnFrame()
+        {
+            // state decision
+            // neural net stuff
+            
+            // state execution
+            switch(currentState)
+            {
+                case CombatUnitState.Idle:
+                    Idle();
+                    break;
+                case CombatUnitState.AttackClosest:
+                    AttackClosest();
+                    break;
+                case CombatUnitState.AttackFastest:
+                    AttackFastest();
+                    break;
+                case CombatUnitState.AttackMostValueable:
+                    AttackMostValueable();
+                    break;
+                case CombatUnitState.AttackWeakest:
+                    AttackWeakest();
+                    break;
+                case CombatUnitState.MoveTowards:
+                    MoveTowards();
+                    break;
+                case CombatUnitState.MoveBack:
+                    MoveBack();
+                    break;
+                case CombatUnitState.Seek:
+                    Seek();
+                    break;
+                case CombatUnitState.Retreat:
+                    Retreat();
+                    break;
+            }
+
+            // fitness function?
+        }
+
+        /// <summary>
+        /// The initial state is idle. Basically the combat unit will wait for the neural net to take over which is commanded by the SquadSupervisor.
+        /// </summary>
+        private void Idle()
+        {
+
+        }
+
+        /// <summary>
+        /// Attack the closest enemy unit.
+        /// </summary>
+        private void AttackClosest()
+        {
+
+        }
+
+        /// <summary>
+        /// Attack the fastest enemy unit.
+        /// </summary>
+        private void AttackFastest()
+        {
+
+        }
+
+        /// <summary>
+        /// Attack the most valueable enemy unit. The value of an unit is based on its training resource costs.
+        /// </summary>
+        private void AttackMostValueable()
+        {
+
+        }
+
+        /// <summary>
+        /// Attack the weakest enemy unit. Being weak depends on all the properties of a unit. So a valueable unit being low on health is considered to be weak compared to a cheap unit with full health.
+        /// </summary>
+        private void AttackWeakest()
+        {
+
+        }
+
+        /// <summary>
+        /// Move towards enemy.
+        /// </summary>
+        private void MoveTowards()
+        {
+
+        }
+
+        /// <summary>
+        /// Back up from enemy.
+        /// </summary>
+        private void MoveBack()
+        {
+
+        }
+
+        /// <summary>
+        /// Seek enemy.
+        /// </summary>
+        private void Seek()
+        {
+
+        }
+
+        /// <summary>
+        /// Retreat from combat.
+        /// </summary>
+        private void Retreat()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ StarcraftNeuralNetAi/source/AiBehavior/SquadSupervisor.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BroodWar;
+using BroodWar.Api;
+using BroodWar.Api.Enum;
+
+namespace NetworkTraining
+{
+    /// <summary>
+    /// The SquadSupervisor is a Singleton and takes care of controlling all combat units. It pretty much assigns the goal to send units to fight.
+    /// </summary>
+    public class SquadSupervisor
+    {
+        #region Member
+        private static SquadSupervisor instance;
+        List<AiCombatUnitBehavior> combatUnits = new List<AiCombatUnitBehavior>();
+        #endregion
+
+        #region Constructor
+        private SquadSupervisor()
+        {
+
+        }
+        #endregion
+
+        #region public functions
+        /// <summary>
+        /// If there is no instance, the supervisor will be instantiated.
+        /// </summary>
+        /// <returns>The only instance of the SquadSupervisor will be returned</returns>
+        public static SquadSupervisor GetInstance()
+        {
+            if(instance == null)
+            {
+                instance = new SquadSupervisor();
+            }
+            return instance;
+        }
+
+        public void AddCombatUnit(AiCombatUnitBehavior unit)
+        {
+            combatUnits.Add(unit);
+        }
+
+        /// <summary>
+        /// This is just a test attack...
+        /// </summary>
+        public void ForceAttack(TilePosition targetPosition)
+        {
+            foreach(AiCombatUnitBehavior unit in combatUnits)
+            {
+                Position pos = new Position(targetPosition.X, targetPosition.Y);
+                unit.GetUnit().Move(pos, false);
+            }
+        }
+
+        // Getter
+        public int GetSquadCount()
+        {
+            return combatUnits.Count;
+        }
+
+        // Setter
+        #endregion
+
+        #region SquadSupervisor logic
+        public void OnFrame()
+        {
+            foreach(AiCombatUnitBehavior combatUnit in combatUnits)
+            {
+                combatUnit.OnFrame();
+            }
+        }
+        #endregion
+    }
+}
